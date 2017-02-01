c26ad9ac69b8f5d6fb024d0838e4b3e1516e70d1
mwarrington <mason.arrington@gmail.com>
2017-02-01 16:23:29 -0600

    Early framework for the dialog system

---
+++ CaduceusQuest/Assets/Scripts/DialogSystem/Convorsation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Convorsation
+{
+    public string Name;
+    public Line[] MyLines;
+    public DialogOptions[] MyDialogOptionsArray;
+
+    private int _currentIndex;
+}
---
+++ CaduceusQuest/Assets/Scripts/DialogSystem/Line.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line
+{
+    public string Speaker;
+    public Vector2 EmotionColor;
+    public int DialogOptionsIndex;
+}
