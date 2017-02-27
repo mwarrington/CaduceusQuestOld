7578f9bd522477912373abe564a45defcd15c0cc
mwarrington <mason.arrington@gmail.com>
2017-02-27 16:07:20 -0600

    Starting on CompSci puzzle

---
+++ CaduceusQuest/Assets/Scripts/EncounterSystem/EncounterPuzzles/CompSciPuzzleManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompSciPuzzleManager : MonoBehaviour
+{
+    public GameObject[] TopRowPositions,
+                        BottomRowPositions;
+                        
+    void Start()
+    {
+        InstantiateTopRow();
+    }
+
+    private void InstantiateTopRow()
+    {
+        GameObject[] SymbolPrefabs = new GameObject[4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            string path = "Prefabs/EncounterPuzzles/CompSciPuzzle/CSsymbol" + (i + 1);
+            SymbolPrefabs[i] = Resources.Load<GameObject>(path);
+        }
+
+        for (int i = 0; i < TopRowPositions.Length; i++)
+        {
+            int rand = Random.Range(0, 4);
+            Instantiate(SymbolPrefabs[rand], TopRowPositions[i].transform.position, Quaternion.identity);
+        }
+    }
+}
