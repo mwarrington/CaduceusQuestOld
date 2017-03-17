db4504bfc064bde29849be8bbcb2cf1c786bde6a
mwarrington <mason.arrington@gmail.com>
2017-03-17 11:11:31 -0500

    A sound manager

---
+++ CaduceusQuest/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource BGMChannel,
+                       AmbientChannel,
+                       HUDSFX,
+                       MiscSFX;
+
+    public void PlayBGM(AudioClip clip)
+    {
+        BGMChannel.clip = clip;
+        BGMChannel.Play();
+    }
+
+    public void PlayAmbient(AudioClip clip)
+    {
+        AmbientChannel.clip = clip;
+        AmbientChannel.Play();
+    }
+
+    public void PlayHUDSFX(AudioClip clip)
+    {
+        HUDSFX.clip = clip;
+        HUDSFX.Play();
+    }
+
+    public void PlayMiscSFX(AudioClip clip)
+    {
+        MiscSFX.clip = clip;
+        MiscSFX.Play();
+    }
+
+    public void PauseBGM()
+    {
+        BGMChannel.Pause();
+    }
+
+    public void PauseAmbient()
+    {
+        AmbientChannel.Pause();
+    }
+
+    public void PauseHUDSFX()
+    {
+        HUDSFX.Pause();
+    }
+
+    public void PauseMiscSFX()
+    {
+        MiscSFX.Pause();
+    }
+}
