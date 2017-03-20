2ac4afd0ff9fe935dafcfc22cdfe1a7638f61e8f
Ash <asparrow@bsd.uchicago.edu>
2017-03-20 11:16:30 -0500

    Revert "Made a new class for the encounter system"
    This reverts commit 0329f2318d10a6e89246b2f13175d789b307ab65.

--- CaduceusQuest/Assets/Scripts/EncounterSystem/EncounterManager.cs
+++ CaduceusQuest/Assets/Scripts/EncounterSystem/EncounterManager.cs
@@ -5,27 +5,11 @@ using UnityEngine;
 public class EncounterManager : MonoBehaviour
 {
     public string EncounterSubject;
+    public float EncounterTrust;
     public List<EncounterController> EncounterControllers = new List<EncounterController>();
-    public List<IndividualGoal> EncounterGoals;
 
     private void CreateTurnOrder()
     {
         //EncounterControllers.Add(???)
-    }
-}
-
-public class IndividualGoal
-{
-    public EncounterActionType MyEAType;
-    public string EAName;
-    public float IndividualTrust;
-    public int ActionCountGoal;
-
-    public IndividualGoal(int actionCountGoal, float individualTrust, string eaName, EncounterActionType myEAType)
-    {
-        MyEAType = myEAType;
-        EAName = eaName;
-        IndividualTrust = individualTrust;
-        ActionCountGoal = actionCountGoal;
     }
 }
