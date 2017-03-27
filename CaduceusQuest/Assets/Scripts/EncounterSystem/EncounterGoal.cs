26c4badee0825c242b736dd9b00fd256848bbe37
mwarrington <mason.arrington@gmail.com>
2017-03-27 15:56:17 -0500

    Setting up scriptable object for encounter data

---
+++ CaduceusQuest/Assets/Scripts/EncounterSystem/EncounterGoal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncounterGoal
+{
+    public string Subject,
+                  ActionName;
+    public float IndividualTrust;
+    public EncounterActionType ActionType;
+}
+
+public class Encounter : ScriptableObject
+{
+    public List<EncounterGoal> EncounterGoals = new List<EncounterGoal>();
+}
+
+public class MakeEncounterObj
+{
+    //Make a context menu that creates new Encounter scriptable objects
+}
--- CaduceusQuest/Assets/Scripts/EncounterSystem/EncounterManager.cs
+++ CaduceusQuest/Assets/Scripts/EncounterSystem/EncounterManager.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class EncounterManager : MonoBehaviour
 {
-    public string EncounterSubject;
-    public float EncounterTrust;
+    public List<EncounterGoal> EncounterGoals = new List<EncounterGoal>();
     public List<EncounterController> EncounterControllers = new List<EncounterController>();
 
     private void CreateTurnOrder()
