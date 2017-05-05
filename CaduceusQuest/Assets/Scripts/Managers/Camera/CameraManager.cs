936b7a2f82bd11b1faeb7998c46d0c436d0b4704
mwarrington <mason.arrington@gmail.com>
2017-05-05 16:19:24 -0500

    Start of new cam manager

---
+++ CaduceusQuest/Assets/Scripts/Managers/Camera/CameraManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera CurrentCamera
+    {
+        get
+        {
+            return _currentCamera;
+        }
+        set
+        {
+            _currentCamera = value;
+        }
+    }
+    private Camera _currentCamera;
+
+    private List<Camera> _allCameras = new List<Camera>();
+    private Dictionary<int, CameraSwitch> _allSwitches = new Dictionary<int, CameraSwitch>();
+
+    private void Start()
+    {
+        _allCameras.AddRange(FindObjectsOfType<Camera>());
+
+        for (int i = 0; i < _allCameras.Count; i++)
+        {
+            CameraSwitch[] currentCamSwitches = _allCameras[i].GetComponentsInChildren<CameraSwitch>();
+
+            for (int j = 0; j < currentCamSwitches.Length; j++)
+            {
+                _allSwitches.Add(i, currentCamSwitches[j]);
+            }
+        }
+    }
+}
