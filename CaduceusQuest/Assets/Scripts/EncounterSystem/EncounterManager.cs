0d33d4204d1a65d98f0523b72651579dba164dde
mwarrington <mason.arrington@gmail.com>
2017-05-30 16:19:40 -0500

    Encounter UI structure

--- CaduceusQuest/Assets/Scripts/EncounterSystem/EncounterManager.cs
+++ CaduceusQuest/Assets/Scripts/EncounterSystem/EncounterManager.cs
@@ -7,8 +7,25 @@ public class EncounterManager : MonoBehaviour
     public List<EncounterGoal> EncounterGoals = new List<EncounterGoal>();
     public List<EncounterController> EncounterControllers = new List<EncounterController>();
 
+    //public GameObject[] SubMenus;
+
     private void CreateTurnOrder()
     {
         //EncounterControllers.Add(???)
+    }
+
+    private void OnGUI()
+    {
+        UIHandler();
+    }
+
+    private void UIHandler()
+    {
+
+    }
+
+    public void ShowSubMenu(GameObject subMenu)
+    {
+
     }
 }
