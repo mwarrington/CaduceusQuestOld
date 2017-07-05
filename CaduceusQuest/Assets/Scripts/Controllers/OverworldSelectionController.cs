4be793e6c8fcd1322876fcc5a1b89fdc488b1fcd
Ash <kayura60@gmail.com>
2017-07-04 22:53:34 -0500

    The Overworld

---
+++ CaduceusQuest/Assets/Scripts/Controllers/OverworldSelectionController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldSelectionController : MonoBehaviour
+{
+
+
+	private string location;
+	public GameObject selectionUI;
+	public string sceneName;
+	private NiceSceneTransition transition;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		transition = GameObject.Find ("NiceSceneTransition").GetComponent<NiceSceneTransition> ();
+	}
+
+	void OnMouseEnter ()
+	{
+
+		selectionUI.SetActive (true);
+	
+	}
+
+
+	void OnMouseExit ()
+	{
+		selectionUI.SetActive (false);
+
+		
+	}
+
+	void OnMouseDown(){
+
+		transition.LoadScene (sceneName);
+
+	}
+
+
+}
