7ac0fc0d4e8621dd825be957831dcb8d2e9058fb
mwarrington <mason.arrington@gmail.com>
2017-09-05 16:07:19 -0500

    start of new event system

---
+++ CaduceusQuest/Assets/Scripts/Managers/Events/ChangeDialog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDialog : EventAction
+{
+    protected override void OnActivate()
+    {
+        base.OnActivate();
+    }
+}
